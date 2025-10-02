c72a8651b9fbe367db62fe622ed9f69d9f342996
mitosoza <msozagonzalez@byupathway.edu>
2025-10-02 17:48:44 -0600

    W04 - Added try/catch error handling

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,93 +1,102 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
-const { asyncHandler } = require('../middleware/errorHandler');
 
-const getAll = asyncHandler(async (req, res) => {
+const getAll = async (req, res) => {
   //#swagger.tags=['Cars']
-  const result = await mongodb.getDatabase().db().collection('cars').find();
-  const cars = await result.toArray();
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(cars);
-});
-
-const getSingle = asyncHandler(async (req, res) => {
-  //#swagger.tags=['Cars']
-  const carId = new ObjectId(req.params.id);
-  const result = await mongodb.getDatabase().db().collection('cars').find({ _id: carId });
-  const cars = await result.toArray();
-
-  if (cars.length === 0) {
-    const error = new Error('Car not found');
-    error.status = 404;
-    throw error;
+  try {
+    const result = await mongodb.getDatabase().db().collection('cars').find();
+    const cars = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(cars);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
+};
 
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(cars[0]);
-});
-
-const createCar = asyncHandler(async (req, res) => {
+const getSingle = async (req, res) => {
   //#swagger.tags=['Cars']
-  const car = {
-    make: req.body.make,
-    model: req.body.model,
-    year: req.body.year,
-    color: req.body.color,
-    cylinders: req.body.cylinders,
-    transmission: req.body.transmission,
-    price: req.body.price
-  };
-  const response = await mongodb.getDatabase().db().collection('cars').insertOne(car);
-  if (response.acknowledged) {
-    res.status(201).json({
-      message: 'Car created successfully',
-      id: response.insertedId
-    });
-  } else {
-    const error = new Error('Failed to create car');
-    error.status = 500;
-    throw error;
-  }
-});
+  try {
+    const carId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('cars').find({ _id: carId });
+    const cars = await result.toArray();
 
-const updateCar = asyncHandler(async (req, res) => {
-  //#swagger.tags=['Cars']
-  const carId = new ObjectId(req.params.id);
-  const car = {
-    make: req.body.make,
-    model: req.body.model,
-    year: req.body.year,
-    color: req.body.color,
-    cylinders: req.body.cylinders,
-    transmission: req.body.transmission,
-    price: req.body.price
-  };
-  const response = await mongodb.getDatabase().db().collection('cars').replaceOne({ _id: carId }, car);
-  if (response.modifiedCount > 0) {
-    res.status(200).json({ message: 'Car updated successfully' });
-  } else if (response.matchedCount === 0) {
-    const error = new Error('Car not found');
-    error.status = 404;
-    throw error;
-  } else {
-    const error = new Error('Failed to update car');
-    error.status = 500;
-    throw error;
-  }
-});
+    if (cars.length === 0) {
+      return res.status(404).json({ error: 'Car not found' });
+    }
 
-const deleteCar = asyncHandler(async (req, res) => {
-  //#swagger.tags=['Cars']
-  const carId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db().collection('cars').deleteOne({ _id: carId });
-  if (response.deletedCount > 0) {
-    res.status(200).json({ message: 'Car deleted successfully' });
-  } else {
-    const error = new Error('Car not found');
-    error.status = 404;
-    throw error;
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(cars[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-});
+};
+
+const createCar = async (req, res) => {
+  //#swagger.tags=['Cars']
+  try {
+    const car = {
+      make: req.body.make,
+      model: req.body.model,
+      year: req.body.year,
+      color: req.body.color,
+      cylinders: req.body.cylinders,
+      transmission: req.body.transmission,
+      price: req.body.price
+    };
+    const response = await mongodb.getDatabase().db().collection('cars').insertOne(car);
+    if (response.acknowledged) {
+      res.status(201).json({
+        message: 'Car created successfully',
+        id: response.insertedId
+      });
+    } else {
+      res.status(500).json({ error: 'Failed to create car' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const updateCar = async (req, res) => {
+  //#swagger.tags=['Cars']
+  try {
+    const carId = new ObjectId(req.params.id);
+    const car = {
+      make: req.body.make,
+      model: req.body.model,
+      year: req.body.year,
+      color: req.body.color,
+      cylinders: req.body.cylinders,
+      transmission: req.body.transmission,
+      price: req.body.price
+    };
+    const response = await mongodb.getDatabase().db().collection('cars').replaceOne({ _id: carId }, car);
+    if (response.modifiedCount > 0) {
+      res.status(200).json({ message: 'Car updated successfully' });
+    } else if (response.matchedCount === 0) {
+      res.status(404).json({ error: 'Car not found' });
+    } else {
+      res.status(500).json({ error: 'Failed to update car' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const deleteCar = async (req, res) => {
+  //#swagger.tags=['Cars']
+  try {
+    const carId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('cars').deleteOne({ _id: carId });
+    if (response.deletedCount > 0) {
+      res.status(200).json({ message: 'Car deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Car not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   getAll,
--- controllers/movies.js
+++ controllers/movies.js
@@ -1,92 +1,101 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
-const { asyncHandler } = require('../middleware/errorHandler');
 
-const getAll = asyncHandler(async (req, res) => {
+const getAll = async (req, res) => {
   //#swagger.tags=['Movies']
-  const result = await mongodb.getDatabase().db().collection('movies').find();
-  const movies = await result.toArray();
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(movies);
-});
-
-const getSingle = asyncHandler(async (req, res) => {
-  //#swagger.tags=['Movies']
-  const movieId = new ObjectId(req.params.id);
-  const result = await mongodb.getDatabase().db().collection('movies').find({ _id: movieId });
-  const movies = await result.toArray();
-
-  if (movies.length === 0) {
-    const error = new Error('Movie not found');
-    error.status = 404;
-    throw error;
+  try {
+    const result = await mongodb.getDatabase().db().collection('movies').find();
+    const movies = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(movies);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
+};
 
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json(movies[0]);
-});
-
-const createMovie = asyncHandler(async (req, res) => {
+const getSingle = async (req, res) => {
   //#swagger.tags=['Movies']
-  const movie = {
-    title: req.body.title,
-    releaseYear: req.body.releaseYear,
-    genre: req.body.genre,
-    director: req.body.director,
-    duration: req.body.duration,
-    rating: req.body.rating
-  };
-  const response = await mongodb.getDatabase().db().collection('movies').insertOne(movie);
-  if (response.acknowledged) {
-    res.status(201).json({
-      message: 'Movie created successfully',
-      id: response.insertedId
-    });
-  } else {
-    const error = new Error('Failed to create movie');
-    error.status = 500;
-    throw error;
-  }
-});
+  try {
+    const movieId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('movies').find({ _id: movieId });
+    const movies = await result.toArray();
 
-const updateMovie = asyncHandler(async (req, res) => {
+    if (movies.length === 0) {
+      return res.status(404).json({ error: 'Movie not found' });
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(movies[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const createMovie = async (req, res) => {
   //#swagger.tags=['Movies']
-  const movieId = new ObjectId(req.params.id);
-  const movie = {
-    title: req.body.title,
-    releaseYear: req.body.releaseYear,
-    genre: req.body.genre,
-    director: req.body.director,
-    duration: req.body.duration,
-    rating: req.body.rating
-  };
-  const response = await mongodb.getDatabase().db().collection('movies').replaceOne({ _id: movieId }, movie);
-  if (response.modifiedCount > 0) {
-    res.status(200).json({ message: 'Movie updated successfully' });
-  } else if (response.matchedCount === 0) {
-    const error = new Error('Movie not found');
-    error.status = 404;
-    throw error;
-  } else {
-    const error = new Error('Failed to update movie');
-    error.status = 500;
-    throw error;
+  try {
+    const movie = {
+      title: req.body.title,
+      releaseYear: req.body.releaseYear,
+      genre: req.body.genre,
+      director: req.body.director,
+      duration: req.body.duration,
+      rating: req.body.rating
+    };
+    const response = await mongodb.getDatabase().db().collection('movies').insertOne(movie);
+    if (response.acknowledged) {
+      res.status(201).json({
+        message: 'Movie created successfully',
+        id: response.insertedId
+      });
+    } else {
+      res.status(500).json({ error: 'Failed to create movie' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-});
+};
 
-const deleteMovie = asyncHandler(async (req, res) => {
+const updateMovie = async (req, res) => {
   //#swagger.tags=['Movies']
-  const movieId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db().collection('movies').deleteOne({ _id: movieId });
-
-  if (response.deletedCount > 0) {
-    res.status(200).json({ message: 'Movie deleted successfully' });
-  } else {
-    const error = new Error('Movie not found');
-    error.status = 404;
-    throw error;
+  try {
+    const movieId = new ObjectId(req.params.id);
+    const movie = {
+      title: req.body.title,
+      releaseYear: req.body.releaseYear,
+      genre: req.body.genre,
+      director: req.body.director,
+      duration: req.body.duration,
+      rating: req.body.rating
+    };
+    const response = await mongodb.getDatabase().db().collection('movies').replaceOne({ _id: movieId }, movie);
+    if (response.modifiedCount > 0) {
+      res.status(200).json({ message: 'Movie updated successfully' });
+    } else if (response.matchedCount === 0) {
+      res.status(404).json({ error: 'Movie not found' });
+    } else {
+      res.status(500).json({ error: 'Failed to update movie' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-});
+};
+
+const deleteMovie = async (req, res) => {
+  //#swagger.tags=['Movies']
+  try {
+    const movieId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('movies').deleteOne({ _id: movieId });
+
+    if (response.deletedCount > 0) {
+      res.status(200).json({ message: 'Movie deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Movie not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   getAll,
